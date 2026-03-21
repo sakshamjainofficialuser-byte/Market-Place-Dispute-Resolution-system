f3d0adbcdd8bca44ffae150d80b1896021e1f9fe
Shubhamyadav93 <yadav934168@gmail.com>
2026-03-21 09:58:04 +0530

    create register route and save the data in the database

--- backend-app/src/Routes/register.js
+++ backend-app/src/Routes/register.js
@@ -1,1 +1,30 @@
 const express = require("express")
+
+const userDataModel = require("../models/userData.model")
+const registerRoute = express.Router()
+
+registerRoute.post("/user",async (req,res) => {
+    try{
+    const userData = req.body
+
+    await userDataModel.create({
+        userName: userData.name,
+        userEmail: userData.email,
+        userPassword: userData.password
+    })
+
+   console.log(userData.name,"user created")
+
+    res.status(200).send({
+        message:`${userData.name} created successfully`
+    })
+} catch (error) {
+    console.log(error)
+    res.status(500).send({
+        message: "Failed to create user",
+        error: error.message
+        })
+}
+}) 
+
+module.exports = registerRoute
