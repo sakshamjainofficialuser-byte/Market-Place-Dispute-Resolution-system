c40beffe16307fcba54f94a9c7e43b222c4b0b8c
Shubhamyadav93 <yadav934168@gmail.com>
2026-04-10 13:02:58 +0530

    removed unwanted comments

--- frontend-app/Pages/Homepage.jsx
+++ frontend-app/Pages/Homepage.jsx
@@ -1,26 +1,3 @@
-// import { useContext } from "react"
-// import { ProductContext } from "../ApiContext/ProductContext"
-
-// function Homepage() {
-
-//     const data = useContext(ProductContext)
-//     console.log(data)
-
-//     return (
-//         <>
-//             {data?.map((item) => (
-//                 <div style={{border:"0.2em solid white",padding:'0.2em'}} key={item._id}>
-//                     <h1>{item.title}</h1>
-//                     <p>${item.price}</p>
-//                     <p>{item.description}</p>
-//                 </div>
-//             ))}
-//         </>
-//     )
-// }
-
-// export default Homepage 
-
 import { useEffect, useState } from "react";
 import axios from "axios";
 import "./Homepage.css";
